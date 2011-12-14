8320f91bcb3b97b1ea5d00bf4f5d6be75019f164
Jason Podgorski <jpodgors@unm.edu>
2011-12-13 17:18:49 -0700

    finished chapter 10

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111213182144) do
+ActiveRecord::Schema.define(:version => 20111213233224) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(:version => 20111213182144) do
     t.integer  "cart_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "quality",    :default => 1
+    t.integer  "quantity",   :default => 1
   end
 
   create_table "products", :force => true do |t|
